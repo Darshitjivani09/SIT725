6093d15ff9ca45a57c0beac756db44d691935599
Darshitjivani09 <s224749274@deakin.edu.au>
2024-12-05 16:48:53 +1100

    week4

--- WEEK - 4/dbConnection.js
+++ WEEK - 4/dbConnection.js
@@ -1,29 +1,28 @@
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://s224749274:Djivani23@cluster0.i3djr.mongodb.net/";
-
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-
-// client.connect();
-// module.exports = client;
-
 const { MongoClient } = require("mongodb");
+
 // Replace the uri string with your connection string.
-const uri = "mongodb+srv://s224749274:Djivani23@cluster0.i3djr.mongodb.net/";
+const uri = `mongodb+srv://s224749274:Djivani23@cluster0.i3djr.mongodb.net/`;
+
 const client = new MongoClient(uri);
+
 async function run() {
   try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
+    const database = client.db('darshitsdb');
+    const user = database.collection('users');
+
+    // Query for a get record of collection
+    const userdata = await user.findOne();
+
+    var mobj = { fName: "Darshit", lName: "jivani" };
+
+    user.insertOne(mobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 line inserted");
+        db.close();
+      });
+    
+
+    console.log(userdata);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
